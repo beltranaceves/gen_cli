1e3430d08125e7da30df87071d7961c62d185530
Beltrán Aceves <beltran.aceves@udc.es>
2024-01-22 22:54:35 +0100

    Update dependencies and fix API dependency with gen_dsl

--- lib/gen_cli/runner.ex
+++ lib/gen_cli/runner.ex
@@ -1,27 +1,7 @@
 defmodule GenCLI.Runner do
   @moduledoc "The gen mix task: `mix help gen`"
 
-  @default_flags ["--install"]
-
   def run(args) do
-    args = args ++ @default_flags
-    [generated_app_name | _] = args ++ @default_flags
-    # Create a new Phoenix project
-    # Force download all its dependencies so phx.gen is available
-    # Mix.Tasks.Phx.New.run(args)
-    # Read and Parse Blueprint
-    gen_elements = GenDSL.Parser.read_blueprint()
-    # Execute commands
-    # File.cd(generated_app_name)
-    # gen_commands = Enum.map(gen_elements, &GenDSL.Parser.struct_to_command/1)
-    # TODO: change this to capture all output and show it at the end
-    # Enum.each(gen_commands, &run_command/1)
-  end
-
-  def run_command(command) do
-    {command_head, command_args} = command |> List.pop_at(0)
-    command = ["phx.gen." <> command_head | command_args]
-    {cmd_output, _} = System.cmd("mix", command)
-    Mix.shell().info(cmd_output)
+    GenDSL.generate_from_filepath(args)
   end
 end
--- mix.exs
+++ mix.exs
@@ -5,8 +5,8 @@ defmodule GenCLI.MixProject do
     [
       app: :gen_cli,
       description: "CLI tool for gen_dsl",
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: "0.1.1",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       source_url: "https://github.com/beltranaceves/gen_cli",
@@ -35,8 +35,10 @@ defmodule GenCLI.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:phx_new, "~> 1.6"},
+      {:phoenix, "~> 1.7"},
       # TODO: check deps and versions
-      {:gen_dsl, path: "../gen_dsl"}
+      {:gen_dsl, path: "../gen_dsl", only: :dev},
+      {:gen_dsl, "~> 0.1.1", only: :prod}
     ]
   end
 end
