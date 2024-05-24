de859728692690b33d7577f72d7f535a91f92ff6
Beltrán Aceves <beltran.aceves@udc.es>
2024-05-24 19:53:51 +0200

    Fixed dependency injection and working escript build

--- lib/gen_cli/runner.ex
+++ lib/gen_cli/runner.ex
@@ -1,7 +1,12 @@
 defmodule GenCLI.Runner do
   @moduledoc "The gen mix task: `mix help gen`"
   def run(args) do
-    Mix.install([{:gen_dsl, "~> 0.2.7"}])
+    # Mix.install([{:gen_dsl, "~> 0.2.8"}], force: true)
     GenDSL.generate_from_filepath(args)
+  end
+
+  # Needed for escript
+  def main(args) do
+    run(args)
   end
 end
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GenCLI.MixProject do
     [
       app: :gen_cli,
       description: "CLI tool for gen_dsl",
-      version: "0.1.6",
+      version: "0.2.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -17,14 +17,15 @@ defmodule GenCLI.MixProject do
         links: %{
           "GitHub" => "https://github.com/beltranaceves/gen_cli"
         }
-      ]
+      ],
+      escript: escript()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :mix, :ex_unit, :hex]
     ]
   end
 
@@ -35,8 +36,12 @@ defmodule GenCLI.MixProject do
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       # TODO: check deps and versions
-      # {:gen_dsl, path: "../gen_dsl", only: :dev}
-      {:gen_dsl, "~> 0.2.7"}
+      # {:gen_dsl, path: "../gen_dsl"}
+      {:gen_dsl, "~> 0.3.1"}
     ]
+  end
+
+  def escript do
+    [main_module: GenCLI.Runner]
   end
 end
